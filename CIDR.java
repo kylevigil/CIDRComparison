8638ec2d9e0bcf1517cd62a0923ba47042f4ca5f
Kyle <kyle.vigil831@gmail.com>
2016-04-13 18:45:31 -0700

    Revert "Initial problem setup"
    This reverts commit dba5cabd422bcaf16939b2b4a73eadf9f5db2123.

--- CIDR.java
+++ CIDR.java
@@ -1,32 +1,6 @@
-import java.util.*;
-
 public class CIDR {
-   private String cidrString;
-   private int ip;
-   private int bitMask;
-
-   public CIDR(String cidr) {
-      cidrString = cidr;
-      StringTokenizer s = new StringTokenizer(cidr, "./");
-
-      ip = 0;
-      //System.out.println(cidr);
-      for (int i = 0; i < 4; i++) {
-         int toInsert = Integer.parseInt(s.nextToken());
-         ip |= (toInsert << (24 - (i * 8)));
-      }
-
-      bitMask = (1 << (32 - Integer.parseInt(s.nextToken()))) - 1;
-      //System.out.println(String.format("0x%X", bitMask));
+   public class CIDR() {
+      
    }
-
-   public boolean contains(CIDR comp) {
-      return (ip | bitMask) == (comp.getIP() | bitMask);
-   }
-
-   public int getIP() { return ip; }
-   public int getBitMask() { return bitMask; }
-
-   public String toString() { return cidrString; }
 
 }
--- CompareSets.java
+++ CompareSets.java
@@ -3,7 +3,7 @@ import java.util.*;
 
 public class CompareSets {
    public static void main(String[] args) {
-      List<List<CIDR>> sets = new ArrayList<>();
+      List<List<String>> sets = new ArrayList<>();
       Scanner s;
 
       if (args.length != 1) {
@@ -19,35 +19,20 @@ public class CompareSets {
       }
 
       while (s.hasNextLine()) {
-         List<CIDR> set = new ArrayList<>();
-         Scanner c = new Scanner(s.nextLine());
-         c.useDelimiter(", ");
-
-         while (c.hasNext())
-            set.add(new CIDR(c.next()));
-
+         List<String> set = new ArrayList<>();
+         set.add(s.nextLine());
          sets.add(set);
       }
-
-      CIDR test = new CIDR("10.10.0.0/8");
-      System.out.println(test.contains(new CIDR("10.20.0.0/16")));
 
       printLists(sets);
    }
 
-   public static void printLists(List<List<CIDR>> sets) {
+   public static void printLists(List<List<String>> sets) {
       int i = 0;
-      for (List<CIDR> set : sets) {
+      for (List<String> set : sets) {
          System.out.print("Set " + ++i + ": {");
-
-         boolean first = false;
-         for (CIDR c : set) {
-            if (first) System.out.print(", ");
-
-            first = true;
-            System.out.print(c);
-         }
-
+         for (String s : set)
+            System.out.print(s);
          System.out.println("}");
       }
    }
